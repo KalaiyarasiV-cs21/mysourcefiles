7b27349770995ba4cbdfc656c2698d39593e1066
KalaiyarasiV.cs21 <kalaiyarasiv.cs21@kascsathy.ac.in>
2023-08-21 04:33:30 +0000

    first commit

--- c/add_recursion.c
+++ c/add_recursion.c
@@ -13,7 +13,7 @@ int main()
      int n1,n2;
      printf("Enter Number 1 :");
      scanf("%d", &n1);
-     printf("Enter Number 2 :");
+     printf("Enter Number 2 : ");
      scanf("%d", &n2);
      int sum = add(n1, n2);
      printf("%d + %d = %d", n1, n2, sum);
